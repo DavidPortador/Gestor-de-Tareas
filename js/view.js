494f0bc4ded72956c8cf3b786e7ef838dcedc803
David Portador <david.raw172@gmail.com>
2022-05-02 23:59:07 -0500

    Ultimos detalles, complementacion y habilitacion de codigo

--- js/view.js
+++ js/view.js
@@ -9,8 +9,6 @@ export default class View {
     this.addTodoForm = new AddTodo();
     this.modal = new Modal();
     this.filters = new Filters();
-    
-
     this.addTodoForm.onClick((title, description) => this.addTodo(title, description));
     this.modal.onClick((id, values) => this.editTodo(id, values));
     this.filters.onClick((filters) => this.filter(filters));
@@ -80,8 +78,10 @@ export default class View {
       <td>${todo.title}</td>
       <td>${todo.description}</td>
       <td class="text-center">
+
       </td>
       <td class="text-right">
+
       </td>
     `;
 
